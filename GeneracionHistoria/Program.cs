af8978c141e15c062ed7407ad9683ee447eaced3
mfuentes85 <mfuentes85@gmail.com>
2024-06-02 10:58:03 -0600

    Generacion de Historias

---
+++ GeneracionHistoria/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.Common;
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Linq;
+using System.ComponentModel.Design;
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Generador de historias";
+
+        //Declaración de Variables
+        string personaje;
+        
+        string Nombre;
+        float Promedio;
+        float TotalPromedio;
+        int i = 0;
+
+        //Creación de listas
+        List<string> personajes = ["Juana la Cubana", "Jack Skeleton", "La rana suertuda", "Pipi Calzas Largas", "Mi vecino de arriba"];
+        List<string> evento = ["posa alegremente con sus gafas favoritas", "baila salsa con la luna", "escucha el triste llorar de las marmotas", "baila tap entre"];
+        List<string> lugar = ["en el infino y más allá", "las tumbas del cementerio", "el circuito interior de la CDMX a la hora pico", "las rocas mas grandes de la playa", "mientras camina hacia el dorado"];
+
+
+
+       
+        
+        
+        //Almacenamiento de datos
+        Alumnos.Add(1));
+
+
+//    Pasos:
+//        Variables y Estructuras de Datos: Declara Listas para personajes, lugares y eventos
+
+//Bucles y Condicionales: Define un bucle que en cada ciclo genere un frase coherente, utilizando los elementos de las lista ejemplo: "Ana encontró un tesoro en el bosque encantado".
+
+//Operadores: Utilizar la clase Random de C# para seleccionar aleatoriamente un elemento de cada lista (personaje, lugar, evento).
